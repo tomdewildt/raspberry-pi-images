0ea55bfa4448c518f4e0d026c87e2521e91e2d62
tomdewildt <32359630+tomdewildt@users.noreply.github.com>
2025-11-21 17:23:38 +0100

    Use config files instead of nmcli

--- scripts/ap/ap.sh
+++ scripts/ap/ap.sh
@@ -7,31 +7,74 @@ apt-get update
 # Install packages
 apt-get install -y network-manager dnsmasq
 
-# Stop services
-systemctl stop NetworkManager
-systemctl stop dnsmasq
+# Create configuration directory
+mkdir -p /etc/NetworkManager/system-connections
 
-# Configure network manager
-nmcli connection add type bridge con-name 'bridge' ifname bridge0 \
-    ipv4.method manual \
-    ipv4.addresses "$network_gateway_ip/24" \
-    ipv4.gateway "$network_gateway_ip"
-nmcli connection add type ethernet slave-type bridge con-name 'ethernet' ifname eth0 master bridge0
-nmcli connection add con-name 'hotspot' \
-    ifname wlan0 type wifi slave-type bridge master bridge0 \
-    wifi.mode ap wifi.ssid "$network_name" wifi-sec.key-mgmt wpa-psk \
-    wifi-sec.proto rsn wifi-sec.pairwise ccmp \
-    wifi-sec.psk "$network_password"
+# Configure bridge connection
+cat <<EOF > /etc/NetworkManager/system-connections/bridge.nmconnection
+[connection]
+id=bridge
+type=bridge
+interface-name=bridge0
+autoconnect=true
+autoconnect-priority=100
 
-# Set connections to autoconnect
-nmcli connection modify 'bridge' connection.autoconnect yes
-nmcli connection modify 'ethernet' connection.autoconnect yes
-nmcli connection modify 'hotspot' connection.autoconnect yes
+[bridge]
+stp=false
 
-# Set the connection priority to ensure proper startup order
-nmcli connection modify 'bridge' connection.autoconnect-priority 100
-nmcli connection modify 'ethernet' connection.autoconnect-priority 50
-nmcli connection modify 'hotspot' connection.autoconnect-priority 50
+[ipv4]
+method=manual
+address1=$network_gateway_ip/24
+gateway=$network_gateway_ip
+
+[ipv6]
+method=disabled
+EOF
+chmod 600 /etc/NetworkManager/system-connections/bridge.nmconnection
+
+# Configure ethernet connection
+cat <<EOF > /etc/NetworkManager/system-connections/ethernet.nmconnection
+[connection]
+id=ethernet
+type=ethernet
+interface-name=eth0
+autoconnect=true
+autoconnect-priority=50
+master=bridge0
+slave-type=bridge
+
+[ethernet]
+EOF
+chmod 600 /etc/NetworkManager/system-connections/ethernet.nmconnection
+
+# Configure hotspot connection
+cat <<EOF > /etc/NetworkManager/system-connections/hotspot.nmconnection
+[connection]
+id=hotspot
+type=wifi
+interface-name=wlan0
+autoconnect=true
+autoconnect-priority=50
+master=bridge0
+slave-type=bridge
+
+[wifi]
+mode=ap
+ssid=$network_name
+
+[wifi-security]
+key-mgmt=wpa-psk
+proto=rsn
+pairwise=ccmp
+psk=$network_password
+
+[ipv4]
+method=disabled
+
+[ipv6]
+method=disabled
+EOF
+chmod 600 /etc/NetworkManager/system-connections/hotspot.nmconnection
 
 # Configure dnsmasq
 cat <<EOF > /etc/dnsmasq.d/bridge.conf
@@ -45,15 +88,3 @@ EOF
 # Enable services
 systemctl enable NetworkManager
 systemctl enable dnsmasq
-
-# Start network manager
-systemctl start NetworkManager
-sleep 5
-
-# Bring up the bridge and connections
-nmcli connection up 'bridge'
-nmcli connection up 'hotspot'
-sleep 3
-
-# Start dnsmasq
-systemctl start dnsmasq
